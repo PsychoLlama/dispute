d5c0bebdf26e7a6425a3c986dec092eb356ef283
Jesse Gibson <JesseTheGibson@gmail.com>
2018-09-30 23:44:44 -0600

    Add function to bring it all together
    There's a new function called `resolveArgv` that does pretty much just
    that. Given the root of the command tree, it takes an argv, figures out
    what command it corresponds to, parses out the options, and leaves the
    rest as subcommand arguments.
    
    I think I'm just about ready to make the main entry file.

---
+++ src/__tests__/argv-resolver.test.js
@@ -0,0 +1,30 @@
+// @flow
+import normalizeConfig from '../normalize-config';
+import resolveInvocation from '../argv-resolver';
+
+const resolve = (config, argv: string[]) =>
+  resolveInvocation(normalizeConfig(config), argv);
+
+describe('argv resolver', () => {
+  it('works without any input', () => {
+    const root = { command() {} };
+    const result = resolve(root, []);
+
+    expect(result).toMatchObject({
+      command: { command: root.command },
+      options: {},
+      args: [],
+    });
+  });
+
+  it('resolves subcommand arguments & options', () => {
+    const options = { silent: { usage: '-s' } };
+    const add = { command() {}, options };
+    const root = { subCommands: { add } };
+    const result = resolve(root, ['-s', 'add', 'pkg']);
+
+    expect(result.command.command).toBe(add.command);
+    expect(result.options).toMatchObject({ silent: true });
+    expect(result.args).toEqual(['pkg']);
+  });
+});
---
+++ src/argv-resolver.js
@@ -0,0 +1,23 @@
+// @flow
+import type { CommandTree } from './normalize-config';
+import resolveSubCommand from './command-resolver';
+import parseArgv from './argv-parser';
+
+/**
+ * This is the function that brings it all together. Not much
+ * logic here, just glue.
+ */
+export default (commandTree: CommandTree, argv: string[]) => {
+  const { command, args: subCommandArgs } = resolveSubCommand(
+    commandTree,
+    argv
+  );
+
+  const { options, args } = parseArgv(command, subCommandArgs);
+
+  return {
+    command,
+    options,
+    args,
+  };
+};
