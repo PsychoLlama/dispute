0f8e1536ae2fa0715b9b9236d036d9cf412cf328
Jesse Gibson <JesseTheGibson@gmail.com>
2018-10-06 11:22:49 -0600

    Fix Jest coverage config
    Those `Can't find coverage info` errors were driving me insane. After
    looking at the config, now having done battle with Jest recently,
    I instantly noticed it was missing the `global` part of the coverage
    section. I wish they'd give a notice about that.

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
 // @flow
 module.exports = {
   testMatch: ['<rootDir>/src/**/__tests__/*.test.js'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx}', '!**/node_modules/**'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.js', '!**/node_modules/**'],
   coverageReporters: ['html'],
   testEnvironment: 'node',
   collectCoverage: true,
   coverageThreshold: {
-    statements: 100,
-    functions: 100,
-    branches: 100,
-    lines: 100,
+    global: {
+      statements: 100,
+      functions: 100,
+      branches: 100,
+      lines: 100,
+    },
   },
 };
