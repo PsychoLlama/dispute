83ebabd544632afb3610998c6224c6cf79de96de
Jesse Gibson <JesseTheGibson@gmail.com>
2018-10-10 08:24:11 -0600

    Add more complete help page generator
    Given a command it'll figure out what you'd need to run on the command
    line to reach it, what arguments you need to pass, and it'll show the
    full list of available options. The next step is showing the full list
    of subcommands, but that should be pretty easy.

--- src/__tests__/generate-help-page.test.js
+++ src/__tests__/generate-help-page.test.js
@@ -1,5 +1,6 @@
 // @flow
 import generateHelpPage, {
+  indent,
   describeOptionUsage,
   describeCommandUsage,
 } from '../generate-help-page';
@@ -13,16 +14,49 @@ const createConfig = config =>
   });
 
 describe('Help page generator', () => {
-  describe('generateHelpPage', () => {
+  describe('generateHelpPage(...)', () => {
     it('returns a string', () => {
       const config = createConfig({});
       const result = generateHelpPage(config.cli);
 
       expect(result).toEqual(expect.any(String));
     });
+
+    it('describes the command in focus', () => {
+      const config = createConfig({
+        args: '<project>',
+        command() {},
+      });
+
+      const result = generateHelpPage(config.cli);
+
+      expect(result).toContain(config.cli.name);
+      expect(result).toContain('<project>');
+    });
+
+    it('shows all the options', () => {
+      const quiet = { usage: '-q, --quiet' };
+      const port = { usage: '-p, --port <number>' };
+      const config = createConfig({
+        options: { quiet, port },
+        command() {},
+      });
+
+      const result = generateHelpPage(config.cli);
+
+      expect(result).toContain(quiet.usage);
+      expect(result).toContain(port.usage);
+    });
+
+    it('skips options if none exist', () => {
+      const config = createConfig({ command() {} });
+      const result = generateHelpPage(config.cli);
+
+      expect(result).not.toContain('Options:');
+    });
   });
 
-  describe('describeCommandUsage', () => {
+  describe('describeCommandUsage(...)', () => {
     it('returns a string', () => {
       const args = '<project> [type...]';
       const config = createConfig({ args, command() {} });
@@ -38,14 +72,23 @@ describe('Help page generator', () => {
 
       expect(result).toMatch(/unit-test$/);
     });
+
+    it('shows the full command path', () => {
+      const run = { command() {} };
+      const subCommands = { run };
+      const config = createConfig({ subCommands });
+      const result = describeCommandUsage(config.cli.subCommands.run);
+
+      expect(result).toContain(`${config.cli.name} run`);
+    });
   });
 
-  describe('describeOptionUsage', () => {
-    const createOption = option => {
+  describe('describeOptionUsage(...)', () => {
+    const createOption = usage => {
       const config = createConfig({
         command() {},
         options: {
-          normalized: option,
+          normalized: { usage },
         },
       });
 
@@ -53,10 +96,52 @@ describe('Help page generator', () => {
     };
 
     it('returns a string', () => {
-      const option = createOption({ usage: '--port' });
+      const option = createOption('--port');
       const result = describeOptionUsage(option);
 
-      expect(result).toContain('--port');
+      expect(result).toBe('--port');
+    });
+
+    it('describes short options', () => {
+      const option = createOption('-p');
+      const result = describeOptionUsage(option);
+
+      expect(result).toBe('-p');
+    });
+
+    it('combines short and long options', () => {
+      const option = createOption('--port, -p');
+      const result = describeOptionUsage(option);
+
+      expect(result).toBe('-p, --port');
+    });
+
+    it('shows required arguments', () => {
+      const option = createOption('-p <port>');
+      const result = describeOptionUsage(option);
+
+      expect(result).toBe('-p <port>');
+    });
+
+    it('shows optional arguments', () => {
+      const option = createOption('-q [bool]');
+      const result = describeOptionUsage(option);
+
+      expect(result).toBe('-q [bool]');
+    });
+  });
+
+  describe('indent(...)', () => {
+    it('indents the given content', () => {
+      const result = indent(2, 'content');
+
+      expect(result).toBe('  content');
+    });
+
+    it('indents across multiple lines', () => {
+      const result = indent(2, 'line1\nline2\nline3');
+
+      expect(result).toBe('  line1\n  line2\n  line3');
     });
   });
 });
--- src/generate-help-page.js
+++ src/generate-help-page.js
@@ -4,16 +4,59 @@ import type { CommandTree } from './normalize-commands';
 type CommandOptions = $PropertyType<CommandTree, 'options'>;
 type CommandOption = $PropertyType<CommandOptions, 'index-key'>;
 
-export default (command: CommandTree) => typeof command;
+export const indent = (offset: number, content: string) => {
+  const indentLevel = Array(offset)
+    .fill(' ')
+    .join('');
 
+  const lines = content.split('\n').map(line => indentLevel + line);
+  return lines.join('\n');
+};
+
+// ['cmd', 'sub', 'command']
+const getCommandPath = (command: CommandTree) => {
+  const path = [command.name];
+  let parent = command;
+
+  while ((parent = parent.parent)) {
+    path.unshift(parent.name);
+  }
+
+  return path;
+};
+
+// "Usage: cmd run <arg1> [arg2]"
 export const describeCommandUsage = (command: CommandTree) => {
   const args = command.args.map(arg => arg.raw).join(' ');
   const spacing = command.args.length ? ' ' : '';
+  const commandPath = getCommandPath(command).join(' ');
 
-  return `Usage: ${command.name}${spacing}${args}`;
+  return `Usage: ${commandPath}${spacing}${args}`;
 };
 
-// WIP
-export const describeOptionUsage = (option: CommandOption) => {
-  return `--${String(option.usage.long)}`;
+const describeOptionArg = ({ required, name }) =>
+  required ? `<${name}>` : `[${name}]`;
+
+// "-o, --option <arg>"
+export const describeOptionUsage = ({ usage }: CommandOption) => {
+  const short = usage.short ? `-${usage.short}` : '';
+  const long = usage.long ? `--${usage.long}` : '';
+  const delimiter = short && long ? ', ' : '';
+  const arg = usage.argument ? ' ' + describeOptionArg(usage.argument) : '';
+
+  return `${short}${delimiter}${long}${arg}`;
 };
+
+export default function generateHelpPage(command: CommandTree) {
+  const commandUsage = describeCommandUsage(command);
+  const options = Object.keys(command.options)
+    .map(option => command.options[option])
+    .reduce((description, option, index) => {
+      const newline = index ? '\n' : '';
+      return description + newline + describeOptionUsage(option);
+    }, '');
+
+  const optionsUsage = options ? `\n\nOptions:\n\n${indent(2, options)}` : '';
+
+  return `${commandUsage}${optionsUsage}`;
+}
