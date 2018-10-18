d8286139ee82b71f0a5a48a9cafe5e7894812335
Jesse Gibson <JesseTheGibson@gmail.com>
2018-10-17 21:34:19 -0600

    Print descriptions in subcommand output
    If the subcommand has an associated description, print it alongside the
    subcommand command name. Format inspired by docker's help output.

--- src/help/__tests__/sub-commands.test.js
+++ src/help/__tests__/sub-commands.test.js
@@ -2,17 +2,20 @@
 import normalizeConfig from '../../normalize-config';
 import describeSubCommands from '../sub-commands';
 
-const createConfig = config =>
-  normalizeConfig({
+const createConfig = config => {
+  const { cli } = normalizeConfig({
     packageJson: { version: '1.2.3' },
     commandName: 'unit-test',
     cli: config,
   });
 
+  return cli.subCommands;
+};
+
 describe('describeSubCommands(...)', () => {
   it('returns an empty string for no subcommands', () => {
     const config = createConfig();
-    const result = describeSubCommands(config.cli.subCommands);
+    const result = describeSubCommands(config);
 
     expect(result).toBe('');
   });
@@ -21,23 +24,39 @@ describe('describeSubCommands(...)', () => {
     const remove = { command() {} };
     const add = { command() {} };
     const config = createConfig({ subCommands: { add, remove } });
-    const result = describeSubCommands(config.cli.subCommands);
+    const result = describeSubCommands(config);
 
     expect(result).toContain('- remove');
     expect(result).toContain('- add');
   });
 
   it('sorts subcommands alphabetically', () => {
-    const add = { command() {} };
-    const branch = { command() {} };
-    const checkout = { command() {} };
+    const add = { command() {}, description: 'Stage a file' };
+    const branch = { command() {}, description: 'Create a branch' };
+    const checkout = { command() {}, description: 'Switch to a branch' };
     const subCommands = { checkout, add, branch };
     const config = createConfig({ subCommands });
-    const result = describeSubCommands(config.cli.subCommands);
+    const result = describeSubCommands(config);
     const lines = result.split('\n');
 
     expect(lines[0]).toContain('add');
     expect(lines[1]).toContain('branch');
     expect(lines[2]).toContain('checkout');
+  });
+
+  it('shows descriptions', () => {
+    const add = { command() {}, description: 'Stage a file' };
+    const config = createConfig({ subCommands: { add } });
+    const result = describeSubCommands(config);
+
+    expect(result).toContain(add.description);
+  });
+
+  it('skips descriptions if omitted', () => {
+    const add = { command() {} };
+    const config = createConfig({ subCommands: { add } });
+    const result = describeSubCommands(config);
+
+    expect(result).not.toMatch(/(null|undefined)/);
   });
 });
--- src/help/sub-commands.js
+++ src/help/sub-commands.js
@@ -1,16 +1,42 @@
 // @flow
 import type { CommandTree } from '../normalize-commands';
 
-// - command1
-// - command2
-// - command3
+const DESCRIPTION_CHAR_PADDING = 5;
+
+// Add padding to command names so they all equal the same length.
+// Ensures descriptions start at the same column.
+const createDescriptionPadding = (commandNames: string[]) => {
+  const commandNameLengths = commandNames.map(name => name.length);
+  const longestCommandNameLength = Math.max(...commandNameLengths, 0);
+  const paddingSize = longestCommandNameLength + DESCRIPTION_CHAR_PADDING;
+
+  const padding = Array(paddingSize)
+    .fill('')
+    .join(' ');
+
+  return (commandName: string) => {
+    const endPadding = padding.slice(commandName.length);
+    return commandName + endPadding;
+  };
+};
+
+const sortAlphabetically = (a, b) => (a > b ? 1 : -1);
+
+// - command            description
+// - longer-command     description
+// - no-description
 export default function describeSubCommands(
   commands: $PropertyType<CommandTree, 'subCommands'>
 ) {
-  return Object.keys(commands)
-    .sort((a, b) => (a > b ? 1 : -1))
-    .reduce((subCommands, commandName, index) => {
-      const newline = index ? '\n' : '';
-      return `${subCommands}${newline}- ${commandName}`;
-    }, '');
+  const commandNames = Object.keys(commands).sort(sortAlphabetically);
+  const padCommandName = createDescriptionPadding(commandNames);
+
+  return commandNames
+    .map(commandName => {
+      const command = commands[commandName];
+      const description = command.description || '';
+
+      return `- ${padCommandName(command.name)} ${description}`;
+    })
+    .join('\n');
 }
